6146f11abe97ae43eae51bd0be7b9a5da00f5481
Karla Santos <105814864+Sark1223@users.noreply.github.com>
2023-04-14 09:18:46 -0700

    Demostración a Amorcito

--- FARMACIA2/Proyecto_FARMACIA/PL/frmMedicamentos.Designer.cs
+++ FARMACIA2/Proyecto_FARMACIA/PL/frmMedicamentos.Designer.cs
@@ -85,7 +85,8 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dgvTablaDB.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvTablaDB.EnableHeadersVisualStyles = false;
-            this.dgvTablaDB.Location = new System.Drawing.Point(21, 108);
+            this.dgvTablaDB.Location = new System.Drawing.Point(32, 166);
+            this.dgvTablaDB.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvTablaDB.Name = "dgvTablaDB";
             dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(88)))), ((int)(((byte)(113)))));
@@ -98,15 +99,16 @@
             this.dgvTablaDB.RowHeadersVisible = false;
             this.dgvTablaDB.RowHeadersWidth = 30;
             this.dgvTablaDB.RowTemplate.Height = 30;
-            this.dgvTablaDB.Size = new System.Drawing.Size(412, 344);
+            this.dgvTablaDB.Size = new System.Drawing.Size(562, 529);
             this.dgvTablaDB.TabIndex = 29;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Proyecto_FARMACIA.Properties.Resources.lupa;
-            this.pictureBox1.Location = new System.Drawing.Point(71, 74);
+            this.pictureBox1.Location = new System.Drawing.Point(106, 114);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(36, 33);
+            this.pictureBox1.Size = new System.Drawing.Size(54, 51);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 40;
             this.pictureBox1.TabStop = false;
@@ -116,9 +118,10 @@
             this.lblInstrucciones.AutoSize = true;
             this.lblInstrucciones.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblInstrucciones.ForeColor = System.Drawing.Color.DimGray;
-            this.lblInstrucciones.Location = new System.Drawing.Point(128, 42);
+            this.lblInstrucciones.Location = new System.Drawing.Point(192, 65);
+            this.lblInstrucciones.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblInstrucciones.Name = "lblInstrucciones";
-            this.lblInstrucciones.Size = new System.Drawing.Size(538, 17);
+            this.lblInstrucciones.Size = new System.Drawing.Size(822, 23);
             this.lblInstrucciones.TabIndex = 39;
             this.lblInstrucciones.Text = "Para MODIFICAR o ELIMINAR cualquier REGRISTRO haga doble click sobre el mismo";
             this.lblInstrucciones.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -128,9 +131,10 @@
             this.lblOpcion.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblOpcion.AutoSize = true;
             this.lblOpcion.Font = new System.Drawing.Font("Cooper Black", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOpcion.Location = new System.Drawing.Point(318, 12);
+            this.lblOpcion.Location = new System.Drawing.Point(477, 18);
+            this.lblOpcion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblOpcion.Name = "lblOpcion";
-            this.lblOpcion.Size = new System.Drawing.Size(254, 31);
+            this.lblOpcion.Size = new System.Drawing.Size(383, 47);
             this.lblOpcion.TabIndex = 36;
             this.lblOpcion.Text = "MEDICAMENTOS";
             // 
@@ -139,9 +143,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(145, 88);
+            this.label1.Location = new System.Drawing.Point(218, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(177, 17);
+            this.label1.Size = new System.Drawing.Size(269, 23);
             this.label1.TabIndex = 41;
             this.label1.Text = "Medicamentos en general";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -179,7 +184,8 @@
             dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle7;
             this.dataGridView1.EnableHeadersVisualStyles = false;
-            this.dataGridView1.Location = new System.Drawing.Point(444, 176);
+            this.dataGridView1.Location = new System.Drawing.Point(626, 271);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
             dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(88)))), ((int)(((byte)(113)))));
@@ -192,15 +198,16 @@
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 30;
             this.dataGridView1.RowTemplate.Height = 30;
-            this.dataGridView1.Size = new System.Drawing.Size(344, 277);
+            this.dataGridView1.Size = new System.Drawing.Size(556, 426);
             this.dataGridView1.TabIndex = 42;
             // 
             // cmdAgregar
             // 
             this.cmdAgregar.Image = global::Proyecto_FARMACIA.Properties.Resources.add;
-            this.cmdAgregar.Location = new System.Drawing.Point(26, 66);
+            this.cmdAgregar.Location = new System.Drawing.Point(39, 102);
+            this.cmdAgregar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cmdAgregar.Name = "cmdAgregar";
-            this.cmdAgregar.Size = new System.Drawing.Size(39, 41);
+            this.cmdAgregar.Size = new System.Drawing.Size(58, 63);
             this.cmdAgregar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.cmdAgregar.TabIndex = 38;
             this.cmdAgregar.TabStop = false;
@@ -208,9 +215,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Proyecto_FARMACIA.Properties.Resources.add;
-            this.pictureBox2.Location = new System.Drawing.Point(444, 138);
+            this.pictureBox2.Location = new System.Drawing.Point(666, 212);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(39, 41);
+            this.pictureBox2.Size = new System.Drawing.Size(58, 63);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 43;
             this.pictureBox2.TabStop = false;
@@ -220,19 +228,20 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(461, 108);
+            this.label2.Location = new System.Drawing.Point(692, 166);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(289, 17);
+            this.label2.Size = new System.Drawing.Size(438, 23);
             this.label2.TabIndex = 44;
             this.label2.Text = "Precio y cantidad existente en la farmacia: ";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmMedicamentos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 481);
+            this.ClientSize = new System.Drawing.Size(1200, 740);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.dataGridView1);
@@ -243,6 +252,7 @@
             this.Controls.Add(this.lblOpcion);
             this.Controls.Add(this.dgvTablaDB);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMedicamentos";
             this.Text = "frmMedicamentos";
             ((System.ComponentModel.ISupportInitialize)(this.dgvTablaDB)).EndInit();
